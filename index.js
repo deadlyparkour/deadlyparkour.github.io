d275800f4d14827ce576ef71333ab7625c011dca
Deadly Parkour Dev <103073150+deadlyparkour@users.noreply.github.com>
2022-04-05 23:24:40 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta property="og:type" content="website">
+<meta property="og:title" content="Deadly Parkour">
+<meta property="og:description" content="Deadly Parkour is a game about farewell, a minimalistic puzzle-platformer where a mysterious character carries a sword deeply tied to him.">
+<meta property="og:url" content="https://deadlyparkour.github.io">
+<meta property="og:image" content="https://media.discordapp.net/attachments/927299411812429845/960478117678448720/IMG_20220404_125651.png">
+    <title>Deadly Parkour</title>
+<link rel="stylesheet" href="style.css">
+  
+<video autoplay loop muted>
+ <source src="http://dl.dropboxusercontent.com/s/odl120pwjgfjp5b/videoplayback%20%289%29.mp4" type="video/mp4">
+</video>
+
+    
+</head>
+<body style="color: crimson; font-weight: bold;">
+ 
+    <img width="700" src="https://media.discordapp.net/attachments/927299411812429845/960455540671131648/unknown-161.png" alt="games">
+   
+    
+    
+
+
+    <p><br></p>
+    
+    <img width="300" src="https://media.discordapp.net/attachments/927299411812429845/960455540876664842/unknown-209.png" alt="dp">
+
+    <h3>Deadly Parkour</h3>
+    <p>
+        <br>
+    Deadly Parkour is a game about farewell, a
+    minimalistic puzzle-platformer where a mysterious character 
+    carries a sword deeply tied to him.
+        <br>
+    </p>
+    <br>
+
+    <img width="300" src="https://media.discordapp.net/attachments/927299411812429845/960456519789469697/unknown-202.png" alt="dp">
+
+    <h3>İnfo;</h3> 
+    <p>
+・A small puzzle-platformer with a minimalistic and touching story about farewell.
+ <br>
+・Discover the way to carry your sword to the end of each screen.
+  <br>
+・Explore an ancient cave with 50 levels that mix platforming skills and puzzles.
+      <br>
+・More than 15 secrets scattered through the cave to be found.
+      <br>
+・Small Game: Main game can be completed in less than 5 minute.
+        <br>
+
+    </p>
+    <br>
+  
+<iframe width="560" height="315" src="https://www.youtube.com/embed/i2hClOWEznM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  
+<br>
+ <p> 
+  <br>
+    <a href="https://cdn.discordapp.com/attachments/925764846790795265/958032465757360128/DeadlyParkourSetup.zip">Click to download the game! 🎉</a>
+  <br>
+  
+  </p>
+  <br>
+  
+    <img width="300" src="https://media.discordapp.net/attachments/927299411812429845/960456520078860298/unknown-128.png" alt="dp">
+    <img width="300" src="https://media.discordapp.net/attachments/927299411812429845/960456520678637608/unknown-83.png" alt="dp">
+    <img width="300" src="https://media.discordapp.net/attachments/927299411812429845/960456520947085332/unknown-190.png" alt="dp">
+   
+      
+    <br>
+
+  
+    
+</body>
+    </html>
